2de2f91b89c6060983e053d02db279f6045caf44
venuszjp <835446988@qq.com>
2018-04-28 12:02:02 +0800

    格式化links插件代码

--- usr/plugins/Links/Action.php
+++ usr/plugins/Links/Action.php
@@ -1,79 +1,80 @@
 <?php
+
 class Links_Action extends Typecho_Widget implements Widget_Interface_Do
 {
-	private $db;
-	private $options;
-	private $prefix;
-			
-	public function insertLink()
-	{
-		if (Links_Plugin::form('insert')->validate()) {
-			$this->response->goBack();
-		}
-		/** 取出数据 */
-		$link = $this->request->from('name', 'url', 'sort', 'image', 'description', 'user');
-		$link['order'] = $this->db->fetchObject($this->db->select(array('MAX(order)' => 'maxOrder'))->from($this->prefix.'links'))->maxOrder + 1;
+    private $db;
+    private $options;
+    private $prefix;
 
-		/** 插入数据 */
-		$link['lid'] = $this->db->query($this->db->insert($this->prefix.'links')->rows($link));
+    public function insertLink()
+    {
+        if (Links_Plugin::form('insert')->validate()) {
+            $this->response->goBack();
+        }
+        /** 取出数据 */
+        $link = $this->request->from('name', 'url', 'sort', 'image', 'description', 'user');
+        $link['order'] = $this->db->fetchObject($this->db->select(array('MAX(order)' => 'maxOrder'))->from($this->prefix . 'links'))->maxOrder + 1;
 
-		/** 设置高亮 */
-		$this->widget('Widget_Notice')->highlight('link-'.$link['lid']);
+        /** 插入数据 */
+        $link['lid'] = $this->db->query($this->db->insert($this->prefix . 'links')->rows($link));
 
-		/** 提示信息 */
-		$this->widget('Widget_Notice')->set(_t('链接 <a href="%s">%s</a> 已经被增加',
-		$link['url'], $link['name']), NULL, 'success');
+        /** 设置高亮 */
+        $this->widget('Widget_Notice')->highlight('link-' . $link['lid']);
 
-		/** 转向原页 */
-		$this->response->redirect(Typecho_Common::url('extending.php?panel=Links%2Fmanage-links.php', $this->options->adminUrl));
-	}
+        /** 提示信息 */
+        $this->widget('Widget_Notice')->set(_t('链接 <a href="%s">%s</a> 已经被增加',
+            $link['url'], $link['name']), null, 'success');
 
-	public function addHannysBlog()
-	{
-		/** 取出数据 */
-		$link = array(
-			'name' => "Hanny's Blog",
-			'url' => "http://www.imhan.com", 
-			'description' => "寒泥 - Typecho插件开发者", 
-		);
-		$link['order'] = $this->db->fetchObject($this->db->select(array('MAX(order)' => 'maxOrder'))->from($this->prefix.'links'))->maxOrder + 1;
+        /** 转向原页 */
+        $this->response->redirect(Typecho_Common::url('extending.php?panel=Links%2Fmanage-links.php', $this->options->adminUrl));
+    }
 
-		/** 插入数据 */
-		$link['lid'] = $this->db->query($this->db->insert($this->prefix.'links')->rows($link));
+    public function addHannysBlog()
+    {
+        /** 取出数据 */
+        $link = array(
+            'name' => "Hanny's Blog",
+            'url' => "http://www.imhan.com",
+            'description' => "寒泥 - Typecho插件开发者",
+        );
+        $link['order'] = $this->db->fetchObject($this->db->select(array('MAX(order)' => 'maxOrder'))->from($this->prefix . 'links'))->maxOrder + 1;
 
-		/** 设置高亮 */
-		$this->widget('Widget_Notice')->highlight('link-'.$link['lid']);
+        /** 插入数据 */
+        $link['lid'] = $this->db->query($this->db->insert($this->prefix . 'links')->rows($link));
 
-		/** 提示信息 */
-		$this->widget('Widget_Notice')->set(_t('链接 <a href="%s">%s</a> 已经被增加',
-		$link['url'], $link['name']), NULL, 'success');
+        /** 设置高亮 */
+        $this->widget('Widget_Notice')->highlight('link-' . $link['lid']);
 
-		/** 转向原页 */
-		$this->response->redirect(Typecho_Common::url('extending.php?panel=Links%2Fmanage-links.php', $this->options->adminUrl));
-	}
+        /** 提示信息 */
+        $this->widget('Widget_Notice')->set(_t('链接 <a href="%s">%s</a> 已经被增加',
+            $link['url'], $link['name']), null, 'success');
 
-	public function updateLink()
-	{
-		if (Links_Plugin::form('update')->validate()) {
-			$this->response->goBack();
-		}
+        /** 转向原页 */
+        $this->response->redirect(Typecho_Common::url('extending.php?panel=Links%2Fmanage-links.php', $this->options->adminUrl));
+    }
 
-		/** 取出数据 */
-		$link = $this->request->from('lid', 'name', 'sort', 'image', 'url', 'description', 'user');
+    public function updateLink()
+    {
+        if (Links_Plugin::form('update')->validate()) {
+            $this->response->goBack();
+        }
 
-		/** 更新数据 */
-		$this->db->query($this->db->update($this->prefix.'links')->rows($link)->where('lid = ?', $link['lid']));
+        /** 取出数据 */
+        $link = $this->request->from('lid', 'name', 'sort', 'image', 'url', 'description', 'user');
 
-		/** 设置高亮 */
-		$this->widget('Widget_Notice')->highlight('link-'.$link['lid']);
+        /** 更新数据 */
+        $this->db->query($this->db->update($this->prefix . 'links')->rows($link)->where('lid = ?', $link['lid']));
 
-		/** 提示信息 */
-		$this->widget('Widget_Notice')->set(_t('链接 <a href="%s">%s</a> 已经被更新',
-		$link['url'], $link['name']), NULL, 'success');
+        /** 设置高亮 */
+        $this->widget('Widget_Notice')->highlight('link-' . $link['lid']);
 
-		/** 转向原页 */
-		$this->response->redirect(Typecho_Common::url('extending.php?panel=Links%2Fmanage-links.php', $this->options->adminUrl));
-	}
+        /** 提示信息 */
+        $this->widget('Widget_Notice')->set(_t('链接 <a href="%s">%s</a> 已经被更新',
+            $link['url'], $link['name']), null, 'success');
+
+        /** 转向原页 */
+        $this->response->redirect(Typecho_Common::url('extending.php?panel=Links%2Fmanage-links.php', $this->options->adminUrl));
+    }
 
     public function deleteLink()
     {
@@ -81,15 +82,15 @@ class Links_Action extends Typecho_Widget implements Widget_Interface_Do
         $deleteCount = 0;
         if ($lids && is_array($lids)) {
             foreach ($lids as $lid) {
-                if ($this->db->query($this->db->delete($this->prefix.'links')->where('lid = ?', $lid))) {
-                    $deleteCount ++;
+                if ($this->db->query($this->db->delete($this->prefix . 'links')->where('lid = ?', $lid))) {
+                    $deleteCount++;
                 }
             }
         }
         /** 提示信息 */
-        $this->widget('Widget_Notice')->set($deleteCount > 0 ? _t('链接已经删除') : _t('没有链接被删除'), NULL,
-        $deleteCount > 0 ? 'success' : 'notice');
-        
+        $this->widget('Widget_Notice')->set($deleteCount > 0 ? _t('链接已经删除') : _t('没有链接被删除'), null,
+            $deleteCount > 0 ? 'success' : 'notice');
+
         /** 转向原页 */
         $this->response->redirect(Typecho_Common::url('extending.php?panel=Links%2Fmanage-links.php', $this->options->adminUrl));
     }
@@ -98,22 +99,22 @@ class Links_Action extends Typecho_Widget implements Widget_Interface_Do
     {
         $links = $this->request->filter('int')->lid;
         if ($links && is_array($links)) {
-			foreach ($links as $sort => $lid) {
-				$this->db->query($this->db->update($this->prefix.'links')->rows(array('order' => $sort + 1))->where('lid = ?', $lid));
-			}
+            foreach ($links as $sort => $lid) {
+                $this->db->query($this->db->update($this->prefix . 'links')->rows(array('order' => $sort + 1))->where('lid = ?', $lid));
+            }
         }
     }
 
-	public function action()
-	{
-		$this->db = Typecho_Db::get();
-		$this->prefix = $this->db->getPrefix();
-		$this->options = Typecho_Widget::widget('Widget_Options');
-		$this->on($this->request->is('do=insert'))->insertLink();
-		$this->on($this->request->is('do=addhanny'))->addHannysBlog();
-		$this->on($this->request->is('do=update'))->updateLink();
-		$this->on($this->request->is('do=delete'))->deleteLink();
-		$this->on($this->request->is('do=sort'))->sortLink();
-		$this->response->redirect($this->options->adminUrl);
-	}
+    public function action()
+    {
+        $this->db = Typecho_Db::get();
+        $this->prefix = $this->db->getPrefix();
+        $this->options = Typecho_Widget::widget('Widget_Options');
+        $this->on($this->request->is('do=insert'))->insertLink();
+        $this->on($this->request->is('do=addhanny'))->addHannysBlog();
+        $this->on($this->request->is('do=update'))->updateLink();
+        $this->on($this->request->is('do=delete'))->deleteLink();
+        $this->on($this->request->is('do=sort'))->sortLink();
+        $this->response->redirect($this->options->adminUrl);
+    }
 }
