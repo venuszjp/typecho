4b9d0744271cf994f1abe42bab2f6b18f6fac908
venuszjp <835446988@qq.com>
2019-02-28 17:29:56 +0800

    撤销测试

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ if (!defined('__TYPECHO_ROOT_DIR__') && !@include_once 'config.inc.php') {
     file_exists('./install.php') ? header('Location: install.php') : print('Missing Config File');
     exit;
 }
-var_dump($_SERVER);exit;
 
 /** 初始化组件 */
 Typecho_Widget::widget('Widget_Init');
